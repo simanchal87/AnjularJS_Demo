6dc97ab89b2d7bc4d063ea468b6e5c4ca9d552b7
sidash <sidash@3INDSIDASH1.ad.here.com>
2019-08-29 19:10:22 +0530

    Print somthing

---
+++ AnjularJS_Demo/src/main/webapp/AnjularCodes.js
@@ -0,0 +1,8 @@
+//reference path="https://ajax.googleapis.com/ajax/libs/angularjs/1.7.8/angular.min.js"/>
+
+var myApp = angular.module("myModule", []);
+
+myApp.controller("myController",function ($scope){
+	$scope.message = "Hello Anjular";
+});
+
--- AnjularJS_Demo/src/main/webapp/index.jsp
+++
@@ -1,5 +0,0 @@
-<html>
-<body>
-<h2>Hello World! as</h2>
-</body>
-</html>
